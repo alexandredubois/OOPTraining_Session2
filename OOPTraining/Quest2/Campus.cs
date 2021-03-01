c9b2c9e3dd2ef96f3a39fd52cf5b4873c69c7a5a
Alexandre Dubois <contact@alexandredubois.com>
2021-03-01 10:58:31 +0100

    Fix wrong word

--- OOPTraining/Quest2/Campus.cs
+++ OOPTraining/Quest2/Campus.cs
@@ -6,6 +6,6 @@
         //TODO 15) Ajouter une propriété Capacité d'accueil (Capacity) dans cette classe
         //QUESTION : Est-il nécessaire de re-déclarer les propriétés Latitude, Longitude, Name ? Pourquoi ?
         //TODO 16) Redéfinir la méthode toString pour qu'elle retourne une chaîne de caractère de la forme (NAME est un campus capable d'accueillir CAPACITY étudiants)
-        //où NAME et BUILDYEAR seront remplacés par les valeurs des propriétés correspondantes
+        //où NAME et CAPACITY seront remplacés par les valeurs des propriétés correspondantes
     }
 }
